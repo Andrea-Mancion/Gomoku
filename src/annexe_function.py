b856e7bf785d1dc71dc8cc1fbf2458b79027b978
Andrea-Mancion <andrea.mancion@epitech.eu>
2024-02-12 22:43:11 +0100

    FIX(Attack): check all cases for the attack win and fix them

--- src/annexe_function.py
+++ src/annexe_function.py
@@ -168,6 +168,44 @@ def placePionV2(board, i, j):
     elif board[i][j] == 1 and board[i - 1][j - 1] == 1 and board[i - 2][j - 2] == 1 and board[i - 3][j - 3] == 0 and board[i - 4][j - 4] == 1:
         print("Found pattern Diagonally 4")
         return True, i - 3, j - 3
+    # other cases
+    elif board[i][j] == 1 and board[i][j + 1] == 1 and board[i][j + 2] == 0 and board[i][j + 3] == 1 and board[i][j + 4] == 1:
+        print("Found pattern horizontally")
+        return True, i, j + 2
+    elif board[i][j] == 1 and board[i + 1][j] == 1 and board[i + 2][j] == 0 and board[i + 3][j] == 1 and board[i + 4][j] == 1:
+        print("Found pattern Vertically")
+        return True, i + 2, j
+    elif board[i][j] == 1 and board[i + 1][j + 1] == 1 and board[i + 2][j + 2] == 0 and board[i + 3][j + 3] == 1 and board[i + 4][j + 4] == 1:
+        print("Found pattern Diagonally 1")
+        return True, i + 2, j + 2
+    elif board[i][j] == 1 and board[i - 1][j + 1] == 1 and board[i - 2][j + 2] == 0 and board[i - 3][j + 3] == 1 and board[i - 4][j + 4] == 1:
+        print("Found pattern Diagonally 2")
+        return True, i - 2, j + 2
+    elif board[i][j] == 1 and board[i + 1][j - 1] == 1 and board[i + 2][j - 2] == 0 and board[i + 3][j - 3] == 1 and board[i + 4][j - 4] == 1:
+        print("Found pattern Diagonally 3")
+        return True, i + 2, j - 2
+    elif board[i][j] == 1 and board[i - 1][j - 1] == 1 and board[i - 2][j - 2] == 0 and board[i - 3][j - 3] == 1 and board[i - 4][j - 4] == 1:
+        print("Found pattern Diagonally 4")
+        return True, i - 2, j - 2
+    # other case
+    elif board[i][j] == 1 and board[i][j + 1] == 0 and board[i][j + 2] == 1 and board[i][j + 3] == 1 and board[i][j + 4] == 1:
+        print("Found pattern horizontally")
+        return True, i, j + 1
+    elif board[i][j] == 1 and board[i + 1][j] == 0 and board[i + 2][j] == 1 and board[i + 3][j] == 1 and board[i + 4][j] == 1:
+        print("Found pattern Vertically")
+        return True, i + 1, j
+    elif board[i][j] == 1 and board[i + 1][j + 1] == 0 and board[i + 2][j + 2] == 1 and board[i + 3][j + 3] == 1 and board[i + 4][j + 4] == 1:
+        print("Found pattern Diagonally 1")
+        return True, i + 1, j + 1
+    elif board[i][j] == 1 and board[i - 1][j + 1] == 0 and board[i - 2][j + 2] == 1 and board[i - 3][j + 3] == 1 and board[i - 4][j + 4] == 1:
+        print("Found pattern Diagonally 2")
+        return True, i - 1, j + 1
+    elif board[i][j] == 1 and board[i + 1][j - 1] == 0 and board[i + 2][j - 2] == 1 and board[i + 3][j - 3] == 1 and board[i + 4][j - 4] == 1:
+        print("Found pattern Diagonally 3")
+        return True, i + 1, j - 1
+    elif board[i][j] == 1 and board[i - 1][j - 1] == 0 and board[i - 2][j - 2] == 1 and board[i - 3][j - 3] == 1 and board[i - 4][j - 4] == 1:
+        print("Found pattern Diagonally 4")
+        return True, i - 1, j - 1
     else:
         victory, x, y = checkVictory(board, 1)
         if victory:
